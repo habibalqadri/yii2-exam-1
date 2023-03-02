178163b9f0733c43f3dcae73b84f4d6f312ad297
= <=>
2023-03-02 11:12:20 +0700

    pergantian database

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=admin3', // PostgreSQL
+            'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=admin2', // PostgreSQL
             'username' => 'admin',  // postgre
             'password' => 'Admin123',   // postgre
             'charset' => 'utf8',
